ebf6cfcaff9da6abdd9325670161fc0e83271592
Guus <guusvandervegt2001@gmail.com>
2021-12-03 10:50:56 +0100

    Solved day 3

--- src/com/aoc/Main.java
+++ src/com/aoc/Main.java
@@ -1,11 +1,12 @@
 package com.aoc;
 
 import com.aoc.days.Day1;
+import com.aoc.days.Day3;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        new Day1();
+        new Day3();
     }
 }
