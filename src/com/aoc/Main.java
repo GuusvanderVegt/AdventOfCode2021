8f4c94fff9b77d56acd34553978cdae33383f0e6
Guus <guusvandervegt2001@gmail.com>
2021-12-06 15:06:06 +0100

    Started with day 4

--- src/com/aoc/Main.java
+++ src/com/aoc/Main.java
@@ -2,11 +2,12 @@ package com.aoc;
 
 import com.aoc.days.Day1;
 import com.aoc.days.Day3;
+import com.aoc.days.Day4;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        new Day3();
+        new Day4();
     }
 }
