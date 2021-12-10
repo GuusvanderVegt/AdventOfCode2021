43471ec83ff10d144b3d7a490e606ef83648b0a7
GuusvanderVegt <guusvandervegt2001@gmail.com>
2021-12-10 16:23:51 +0100

    Finished day 10

--- src/com/aoc/days/Day10.java
+++ src/com/aoc/days/Day10.java
@@ -11,12 +11,13 @@ public class Day10 extends AOC {
 
     @Override
     public void solve(List<String> input) {
+        List<String> incompleteLines = new ArrayList<>(input);
         List<Character> closingTags = new ArrayList<>(Arrays.asList(']', '}', '>', ')'));
 
         Stack<Character> openingTags = new Stack<>();
         List<Character> corruptedTags = new ArrayList<>();
 
-        List<Integer> scores = new ArrayList<>();
+        List<Long> scores = new ArrayList<>();
 
         for (int j = 0; j < input.size(); j++) {
             String line = input.get(j);
@@ -28,14 +29,10 @@ public class Day10 extends AOC {
                     char lastOpeningTag = openingTags.pop();
 
                     //Part 1
-//                    if((character == ']' && lastOpeningTag != '[') || (character == '}' && lastOpeningTag != '{') || (character == ')' && lastOpeningTag != '(') || (character == '>' && lastOpeningTag != '<')){
-//                        corruptedTags.add(character);
-//                        break;
-//                    }
-
-                    //Part 2
                     if((character == ']' && lastOpeningTag != '[') || (character == '}' && lastOpeningTag != '{') || (character == ')' && lastOpeningTag != '(') || (character == '>' && lastOpeningTag != '<')){
-                        input.remove(line);
+                        corruptedTags.add(character);
+                        incompleteLines.remove(line);
+                        break;
                     }
 
                 } else {
@@ -43,11 +40,12 @@ public class Day10 extends AOC {
                 }
             }
 
+            openingTags = new Stack<>();
         }
 
         openingTags = new Stack<>();
 
-        for(String line : input){
+        for(String line : incompleteLines){
             for (int i = 0; i < line.length(); i++) {
                 char character = line.charAt(i);
 
@@ -68,55 +66,38 @@ public class Day10 extends AOC {
             openingTags = new Stack<>();
         }
 
-
-
-        System.out.println(scores.size() / 2);
-
         Collections.sort(scores);
-        System.out.println(scores.get((scores.size()/2) - 1));
-
-//        int solPart1 = 0;
-
-//        for(char c : corruptedTags){
-//            switch(c) {
-//                case ')' -> solPart1 += 3;
-//                case ']' -> solPart1 += 57;
-//                case '}' -> solPart1 += 1197;
-//                case '>' -> solPart1 += 25137;
-//            }
-//        }
-//
-//        System.out.println(solPart1);
+        System.out.println(scores.get((scores.size()/2)));
 
     }
 
-    private Integer calculateScore(Stack<Character> openingTags) {
+    private Long calculateScore(Stack<Character> openingTags) {
         char tag;
-        int total = 0;
+        long total = 0;
 
-        for (int i = 0; i < openingTags.size(); i++) {
+        while(openingTags.size() > 0) {
             tag = openingTags.pop();
+            total *= 5;
 
             if(tag == '['){
-                total += (total * 5) + 2;
+                total += 2;
             }
 
             if(tag == '{'){
-                total += (total * 5) + 3;
+                total += 3;
 
             }
 
             if(tag == '('){
-                total += (total * 5) + 1;
+                total += 1;
 
             }
 
             if(tag == '<'){
-                total += (total * 5) + 4;
+                total += 4;
 
             }
         }
-
         return total;
     }
 }
