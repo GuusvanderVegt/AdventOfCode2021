5ea77a3a8b0e094638054abbeb97719b4df4a4f6
Guus <guusvandervegt2001@gmail.com>
2021-12-15 10:43:13 +0100

    Day 12 almost complete

--- src/com/aoc/days/Day12.java
+++ src/com/aoc/days/Day12.java
@@ -9,18 +9,102 @@ public class Day12 extends AOC {
         super("12");
     }
 
+    private Map<Vertex, List<Vertex>> map;
+    public Map<String, List<String>> paths;
+
+
     @Override
     public void solve(List<String> input) {
-        Graph g = new Graph();
-
-        for(String line : input){
-            String[] caves = line.split("-");
-            g.addEdge(caves[0], caves[1]);
+        paths = new HashMap<>();
+        for(String s : input)
+        {
+            String[] pathParts = s.trim().split("-");
+            paths.computeIfAbsent(pathParts[0], k -> new ArrayList<>()).add(pathParts[1]);
+            paths.computeIfAbsent(pathParts[1], k -> new ArrayList<>()).add(pathParts[0]);
         }
+
+        //Part 1
+        System.out.println(navigate("start", new ArrayList<>(), false).size());
+        //Part 2
+        List<List<String>> part2List = navigate("start", new ArrayList<>(), true);
+
+        for(int i = part2List.size() - 1; i >= 0; i--)
+        {
+            List<String> l1 = part2List.get(i);
+            boolean remove = false;
+            for(int j = part2List.size() - 1; j >= 0; j--)
+            {
+                if(i == j)
+                    continue;
+                List<String> l2 = part2List.get(j);
+                if(l1.equals(l2))
+                {
+                    remove = true;
+                    break;
+                }
+            }
+
+            if(remove)
+                part2List.remove(i);
+        }
+        System.out.println(part2List.size());
+
+    }
+
+    public List<List<String>> navigate(String pos, List<String> visited, boolean allowTwice)
+    {
+        if(pos.equalsIgnoreCase("end"))
+        {
+            List<String> one = new ArrayList<>();
+            one.add("end");
+            List<List<String>> two = new ArrayList<>();
+            two.add(one);
+            return two;
+        }
+
+        List<List<String>> toReturn = new ArrayList<>();
+
+        if(allowTwice && !pos.equals("start"))
+        {
+            for(String dest : paths.get(pos))
+            {
+                if(dest.charAt(0) >= 'a' && dest.charAt(0) <= 'z' && visited.contains(dest))
+                    continue;
+                List<List<String>> toAdd = navigate(dest, new ArrayList<>(visited), false);
+                for(int i = toAdd.size() - 1; i >= 0; i--)
+                {
+                    boolean remove = true;
+                    for(String s : toAdd.get(i))
+                    {
+                        if(s.equals(pos))
+                        {
+                            remove = false;
+                            break;
+                        }
+                    }
+
+                    if(remove)
+                        toAdd.remove(i);
+                }
+                toReturn.addAll(toAdd);
+            }
+        }
+
+        visited.add(pos);
+        for(String dest : paths.get(pos))
+        {
+            if(dest.charAt(0) >= 'a' && dest.charAt(0) <= 'z' && visited.contains(dest))
+                continue;
+            toReturn.addAll(navigate(dest, new ArrayList<>(visited), allowTwice));
+        }
+
+        for(List<String> sp : toReturn)
+            sp.add(pos);
+
+        return toReturn;
     }
 
     public class Graph {
-//        private ArrayList<Vertex>[] adjList;
        private Map<Vertex, List<Vertex>> map = new HashMap<>();
 
        public void addVertex(Vertex s){
